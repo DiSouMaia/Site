c59703499369d14188e46c473085dad17ff4582e
DiSouMaia <144067521+DiSouMaia@users.noreply.github.com>
2023-09-12 20:30:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tiktok = document.getElementById('meuBotao');
+    
+    meuBotao.addEventListener('click', function () {
+        // Insira o código que deseja executar quando o botão for clicado aqui
+        alert('https://grabify.link/VK5RWM');
+    });
+});
